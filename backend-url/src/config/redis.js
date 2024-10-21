3789b63cd9e6953e24df06b0f132948f7c3d81b8
bhuvan chandra <bhuvanchandrad@gmail.com>
2024-10-21 23:16:26 +0530

    Removed Url-backend directory

---
+++ backend-url/src/config/redis.js
@@ -0,0 +1,19 @@
+// const redis = require("redis");
+
+// const redisClient = redis.createClient({
+//     host: "localhost",
+//     port: "6379",
+//     legacyMode: true
+// });
+
+// redisClient.on("connect", ()=>{
+//      console.log("connected to redis")
+// });
+
+// redisClient.on("error", (err)=>{
+//     console.log("redis connection error", err)
+// });
+
+// redisClient.connect();
+
+// module.exports = redisClient;
