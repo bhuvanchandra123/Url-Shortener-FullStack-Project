03234fb13d9fd2d99c39b6f7699fc21c109a1f25
bhuvan chandra <bhuvanchandrad@gmail.com>
2024-10-21 23:10:53 +0530

    Removed Url-backend submodule

--- Url-backend/src/routes/url.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createShortURL, redirectOriginalUrl } = require("../controller/url");
-const router = express.Router();
-
-router.post("/shorten", createShortURL)
-router.get("/:urlCode", redirectOriginalUrl)
-
-
-module.exports = router
